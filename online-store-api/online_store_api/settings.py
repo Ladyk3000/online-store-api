07534c6b996f4c270f22c519acc56a7a771cc9f3
Nikolay Ladykin <ladykinnikolay@gmail.com>
2022-07-05 22:06:28 +0300

    add working db

--- online-store-api/online_store_api/settings.py
+++ online-store-api/online_store_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'online_store_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
--- online-store-api/products/models.py
+++ online-store-api/products/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
 	Category_Id = models.AutoField(primary_key = True)
-	Category_Parent_Id = models.IntegerField()
+	Category_Parent_Id = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
 	Category_Name = models.ChatField(max_length = 100)
 
 class Product(model.Model):
-	Product_Category_Id = models.AutoField(primary_key = True)
+	Product_Model = models.AutoField(primary_key = True)
 	Product_Name = models.CharField(max_length = 100)
+	Product_Category_Id = models.ForeignKey('Product', on_delete=models.CASCADE, null=True)
 	Product_VendorCode = models.CharField(max_length = 100)
-	Product_Code = models.AutoField()
-	Product_Description  = models.CharField(max_length = 500)
+	Product_Description = models.CharField(max_length = 500)
 
 class Offer(models.Model):
 	Offer_Price = models.FloatField()
 	Offer_Id = models.AutoField(primary_key = True)
 	Offer_Price_begin  = models.FloatField()
 	Offer_size = models.IntegerField()
-	Offer_model = models.AutoField()
+	Offer_model = models.ForeignKey('Product')
 
 
